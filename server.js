f8623d68c7130ac1c13beb7067563c0c01dffd22
Tryo Asnafi <tryoasnafi@gmail.com>
2021-05-15 18:34:39 +0700

    Add basic hapi server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Haloo, kita akan belajar membuat server menggunakan Hapi");
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World";
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
